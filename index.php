092689c29a47992caf187928874635d5ef5eca7d
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-19 15:14:35 +0200

    Aggiunta array partite basket

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<!-- 1) Creo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. -->
+<?php 
+    $partite = [
+        [
+            "squadraCasa" => "Boston Celtics",
+            "squadraOspite" => "Chicago Bulls",
+            "puntiCasa" => 20,
+            "puntiOspite" => 30
+        ],
+        [
+            "squadraCasa" => "Atlanta Hawks",
+            "squadraOspite" => "Miami Heat",
+            "puntiCasa" => 40,
+            "puntiOspite" => 60
+        ],
+        [
+            "squadraCasa" => "Toronto Raptors",
+            "squadraOspite" => "Washington Wizards",
+            "puntiCasa" => 50,
+            "puntiOspite" => 10
+        ]
+    ]
+?>
