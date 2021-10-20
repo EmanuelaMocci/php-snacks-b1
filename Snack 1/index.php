fdfd00aad9b8009de6fd4f9875b1b1ef83592d26
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-20 10:40:48 +0200

    Aggiunta html snack 1

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -29,7 +29,27 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
     ];
 
     // 2) Stampo a schermo tutte le partite
-    for ($i = 0; $i<count($partite); $i++){
-        echo $partite[$i]["squadraCasa"] . "-" . $partite[$i]["squadraOspite"] . " " . "|" . " " . $partite[$i]["puntiCasa"] . "-" . $partite[$i]["puntiOspite"] . "<br>";
-    }
+    // for ($i = 0; $i<count($partite); $i++){
+    //     echo $partite[$i]["squadraCasa"] . "-" . $partite[$i]["squadraOspite"] . " " . "|" . " " . $partite[$i]["puntiCasa"] . "-" . $partite[$i]["puntiOspite"] . "<br>";
+    // }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Le partite di oggi: </h1>
+    <ul>
+        <!-- 2) Stampo a schermo tutte le partite -->
+        <?php for ($i = 0; $i < count($partite); $i++) { ?>
+
+         <li><?php echo "{$partite[$i]["squadraCasa"]}  -  {$partite[$i]["squadraOspite"]} | {$partite[$i]["puntiCasa"]}  -  {$partite[$i]["puntiOspite"]}"; ?></li>
+        
+        <?php } ?>
+    </ul>
+</body>
+</html>
