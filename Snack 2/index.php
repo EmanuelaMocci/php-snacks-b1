9356a132158490d25f142824c4f27a6ddc47292c
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-20 10:40:25 +0200

    Aggiunta controllo dati Snack 2

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -5,14 +5,27 @@ che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocci
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php 
+// Se non è vuoto email, name e age
     if( !empty($_GET['email']) && !empty($_GET['name']) && !empty($_GET['age'])){
         $email = $_GET['email']; 
         $name = $_GET['name']; 
         $age = $_GET['age'];
+
         if (strpos($email, '@') && strpos($email, '.') && strlen($name) > 3 && is_numeric($age)){
             echo "Accesso riuscito";
         }
     } else {
         echo "Accesso negato";
+    }
+
+    // Versione con il controllo dei dati
+    if (strlen($name) < 3){
+        echo "Accesso negato. La lunghezza minima del nome deve essere di 3 caratteri";
+    } elseif (strpos($email, '@') == false || strpos($email, '.') == false) {
+        echo "Accesso negato. Email formattata male.";
+    } elseif (is_numeric($age) == false) {
+        echo "Accesso negato. L'età deve essere un numero.";
+    } else {
+        echo "Accesso riuscito.";
     }
 ?>
